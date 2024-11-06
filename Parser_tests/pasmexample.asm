84e8f5c3bc72a4a06c52c2431a47ada0c7bfaf6e
moskwitto <115109283+moskwitto@users.noreply.github.com>
2024-11-06 14:26:25 +0100

    Update pasmexample.asm

--- Parser_tests/pasmexample.asm
+++ Parser_tests/pasmexample.asm
@@ -3,12 +3,12 @@
 ;
 ; The semicolon starts a comment that lasts until the end of line.
 
-VIA_BASE = 0x4000  ; Variables can be defined that way: name, equal sign, expression.
+VIA_BASE = 0x8000  ; Variables can be defined that way: name, equal sign, expression.
 VIA_PORTB = VIA_BASE
 VIA_DDRB = VIA_BASE + 2  ; The expression can contain numbers, variables and operations like plus or times.
                          ; Numbers can be input in decimal (1234), hexadecimal (0x4002), or binary (0b10010101) format.
 
-section text origin 0x8000     ; The section directive tell the compiler, where to put the machine code or data that comes next.
+section text origin 0x4000     ; The section directive tell the compiler, where to put the machine code or data that comes next.
                                ; Traditionally the section where your code goes is called "text"
 
 start:                         ; Another way to define variables is by a label: it is a name followed by a colon.
@@ -18,7 +18,7 @@ loop:  mov [VIA_PORTB], a      ; The label can be on the same line as the instru
        inc a
        jmp loop                ; Create an infinite loop.
 
-section data  origin 0x8100
+section data  origin 0x4100
 var1:  db  0xff                ; We just put 0xff into address 0x8100, to be read in at the start.
 
 section vectors origin 0xfffc  ; The section for the reset vectors.
